915fe34a531a661ef6160bd1d5def833a9220e9d
jenoh <memint.contact@gmail.com>
2021-11-11 16:07:13 +0100

    initial commit

---
+++ src/main/java/jenoh/StravaDiscordBot/StravaDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package jenoh.StravaDiscordBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jenoh/StravaDiscordBot/StravaDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package jenoh.StravaDiscordBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
